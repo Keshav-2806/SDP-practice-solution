89cc601293f71191593582df694fe59635bc7c31
Keshav-2806 <keshawkumar234@gmail.com>
2021-03-07 19:55:37 +0530

    answer to Question8

---
+++ Question8.js
@@ -0,0 +1,8 @@
+function relationToLuke(name) {
+	let x={ 'Darth Vader':"Luke, I am your father.",
+				 'Leia':"Luke, I am your sister.",
+				 'Han':"Luke, I am your brother in law.",
+				 'R2D2':"Luke, I am your droid."};
+	return x[name];
+	
+}
