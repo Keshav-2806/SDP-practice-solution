15d38d9843e20e93dd38a2b74dc9ebb9eaefc56f
Keshav-2806 <keshawkumar234@gmail.com>
2021-03-07 17:00:17 +0530

    answer to question2

--- question2.js
+++ question2.js
@@ -1,10 +1,3 @@
-function getVote(votes){
-    return votes={
-        upvotes:upvotes,
-        downvotes:downvotes
-    }
+function getVoteCount(votes) {
+	return votes.upvotes-votes.downvotes;
 }
-let user= getVote(13,0);
-let user1=getVote(2,33);
-console.log(user.upvotes-user.downvotes);
-console.log(user1.upvotes-user1.downvotes);
