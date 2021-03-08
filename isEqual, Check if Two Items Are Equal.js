b4e8384b46e47f75b46fa5f1400b08300932498c
Keshav-2806 <keshawkumar234@gmail.com>
2021-03-08 17:26:38 +0530

    answer to isEqual, Check if Two Items Are Equal.js

---
+++ isEqual, Check if Two Items Are Equal.js
@@ -0,0 +1,9 @@
+//Question link- https://edabit.com/challenge/CqnX6mDnFG9tK4Xj2
+function isEqual(a,b){
+    if(a==b){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(isEqual(2,2))
