164f108d4ad05ba06a3b23d52ebaedd7d40035df
Keshav-2806 <keshawkumar234@gmail.com>
2021-03-08 13:24:37 +0530

    answer to 50-30-20 Strategy

---
+++ 50-30-20 Strategy.js
@@ -0,0 +1,8 @@
+//Question link - https://edabit.com/challenge/MQL7KSftPQzrxdJw6
+function fiftyThirtyTwenty(ati) {
+
+	return {
+		"Needs":(ati*.5),"Wants":(ati*.3),"Savings":(ati*.2)
+	};
+}
+console.log(fiftyThirtyTwenty(10000));
