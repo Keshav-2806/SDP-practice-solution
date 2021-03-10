f5d12360b0872fe34432a79825665a803f745978
Keshav-2806 <keshawkumar234@gmail.com>
2021-03-10 21:37:43 +0530

    answer to Converting Objectsto Arrays.js

---
+++ Converting Objectsto Arrays.js
@@ -0,0 +1,12 @@
+//Question link-  https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
+function toArray(obj) {
+    let x=Object.entries(obj);
+    return x;
+	
+}
+let obj={ a: 1, b: 2 };
+console.log(toArray(obj));
+let obj1={ shrimp: 15, tots: 12 };
+console.log(toArray(obj1));
+let obj2={};
+console.log(toArray(obj2));
