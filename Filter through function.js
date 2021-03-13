ea7d53fbd807af0f455508ac8e9b066e6fc8898d
Keshav-2806 <keshawkumar234@gmail.com>
2021-03-13 21:02:10 +0530

    ans to Filter through function.js

---
+++ Filter through function.js
@@ -0,0 +1,29 @@
+
+// .. your code for inBetween and inArray 
+/*
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+
+console.log( arr.filter(inBetween(3, 6)) ); 
+
+console.log( arr.filter(inArray([1, 2, 10])) ); */
+//Filter inBetween
+function inBetween(a,b){
+    return function(x){
+       return x>=a && x<=b;
+     }
+ 
+}
+let arr=[1, 2, 3, 4, 5, 6, 7];
+console.log(arr.filter(inBetween(3,6)));
+
+
+//Filter inArray
+function inArray(arr){
+    return function(x){
+        return arr.includes(x);
+    };
+
+}
+let arr1=[1,2,3,4,5,6,7];
+console.log(arr1.filter(inArray([1,2,10])));
