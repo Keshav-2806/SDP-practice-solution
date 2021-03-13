a79294d62577aa45eb94eb5e0f6ee1afcc787653
Keshav-2806 <keshawkumar234@gmail.com>
2021-03-13 20:54:48 +0530

    ans to Function in if.js

---
+++ Function in if.js
@@ -0,0 +1,15 @@
+//Look at the code. What will be the result of the call at the last line?
+
+let phrase = "Hello";
+
+if (true) {
+  let user = "John";
+
+  function sayHi() {
+    console.log(`${phrase}, ${user}`);
+  }
+  
+}
+sayHi();
+
+
